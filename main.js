f8b4b002125f50e691ad88a43b492775f7c8a836
maazfatima21 <167995096+maazfatima21@users.noreply.github.com>
2024-04-26 16:39:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,231 @@
+$(document).ready(function ($) {
+    "use strict";
+
+
+    var book_table = new Swiper(".book-table-img-slider", {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        speed: 2000,
+        effect: "coverflow",
+        coverflowEffect: {
+            rotate: 3,
+            stretch: 2,
+            depth: 100,
+            modifier: 5,
+            slideShadows: false,
+        },
+        loopAdditionSlides: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+
+    var team_slider = new Swiper(".team-slider", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        speed: 2000,
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1.2,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            992: {
+                slidesPerView: 3,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+        },
+    });
+
+    jQuery(".filters").on("click", function () {
+        jQuery("#menu-dish").removeClass("bydefault_show");
+    });
+    $(function () {
+        var filterList = {
+            init: function () {
+                $("#menu-dish").mixItUp({
+                    selectors: {
+                        target: ".dish-box-wp",
+                        filter: ".filter",
+                    },
+                    animation: {
+                        effects: "fade",
+                        easing: "ease-in-out",
+                    },
+                    load: {
+                        filter: ".all, .breakfast, .lunch, .dinner",
+                    },
+                });
+            },
+        };
+        filterList.init();
+    });
+
+    jQuery(".menu-toggle").click(function () {
+        jQuery(".main-navigation").toggleClass("toggled");
+    });
+
+    jQuery(".header-menu ul li a").click(function () {
+        jQuery(".main-navigation").removeClass("toggled");
+    });
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    var elementFirst = document.querySelector('.site-header');
+    ScrollTrigger.create({
+        trigger: "body",
+        start: "30px top",
+        end: "bottom bottom",
+
+        onEnter: () => myFunction(),
+        onLeaveBack: () => myFunction(),
+    });
+
+    function myFunction() {
+        elementFirst.classList.toggle('sticky_head');
+    }
+
+    var scene = $(".js-parallax-scene").get(0);
+    var parallaxInstance = new Parallax(scene);
+
+
+});
+
+
+jQuery(window).on('load', function () {
+    $('body').removeClass('body-fixed');
+
+    //activating tab of filter
+    let targets = document.querySelectorAll(".filter");
+    let activeTab = 0;
+    let old = 0;
+    let dur = 0.4;
+    let animation;
+
+    for (let i = 0; i < targets.length; i++) {
+        targets[i].index = i;
+        targets[i].addEventListener("click", moveBar);
+    }
+
+    // initial position on first === All 
+    gsap.set(".filter-active", {
+        x: targets[0].offsetLeft,
+        width: targets[0].offsetWidth
+    });
+
+    function moveBar() {
+        if (this.index != activeTab) {
+            if (animation && animation.isActive()) {
+                animation.progress(1);
+            }
+            animation = gsap.timeline({
+                defaults: {
+                    duration: 0.4
+                }
+            });
+            old = activeTab;
+            activeTab = this.index;
+            animation.to(".filter-active", {
+                x: targets[activeTab].offsetLeft,
+                width: targets[activeTab].offsetWidth
+            });
+
+            animation.to(targets[old], {
+                color: "#0d0d25",
+                ease: "none"
+            }, 0);
+            animation.to(targets[activeTab], {
+                color: "#fff",
+                ease: "none"
+            }, 0);
+
+        }
+
+    }
+});
+
+// Function to add items to the card
+function addToCard(index) {
+    // Get the item details
+    var item = {
+        name: document.querySelectorAll('.title')[index].textContent,
+        price: parseFloat(document.querySelectorAll('.price')[index].textContent.replace(',', '')),
+        image: document.querySelectorAll('.item img')[index].getAttribute('src')
+    };
+
+    // Add item to the card
+    addItemToCard(item);
+}
+
+// Function to add item to the card
+function addItemToCard(item) {
+    // Get the list element
+    var listCard = document.querySelector('.listCard');
+
+    // Create list item
+    var listItem = document.createElement('li');
+    listItem.innerHTML = `
+        <img src="${item.image}">
+        <div class="itemDetails">
+            <div class="itemName">${item.name}</div>
+            <div class="itemPrice">${item.price}</div>
+        </div>
+        <button class="removeItem">Remove</button>
+    `;
+
+    // Add item to the card
+    listCard.appendChild(listItem);
+
+    // Update total
+    updateTotal();
+}
+
+// Function to update total
+function updateTotal() {
+    var total = 0;
+    var prices = document.querySelectorAll('.itemPrice');
+    for (var i = 0; i < prices.length; i++) {
+        total += parseFloat(prices[i].textContent);
+    }
+    document.querySelector('.total').textContent = total;
+}
+
+// Event listener for closing the shopping card
+document.querySelector('.closeShopping').addEventListener('click', function() {
+    document.querySelector('.card').style.display = 'none';
+});
+
+// Event delegation for removing item from the card
+document.querySelector('.listCard').addEventListener('click', function(e) {
+    if (e.target.classList.contains('removeItem')) {
+        e.target.parentElement.remove();
+        updateTotal();
+    }
+});
